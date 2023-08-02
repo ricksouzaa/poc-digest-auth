7cafcdd18ce6dabbf10a0fd70a846fea31969ea8
Ricardo Antonio de Souza <ricardo.souza@clickbus.com>
2023-08-01 22:43:50 -0400

    Improvements

--- src/main/java/poc/digest/auth/controllers/TestController.java
+++ src/main/java/poc/digest/auth/controllers/TestController.java
@@ -1,5 +1,6 @@
 package poc.digest.auth.controllers;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
   @GetMapping
-  String test() {
-    return "OK";
+  String test(Authentication authentication) {
+    return authentication.getName();
   }
 }
